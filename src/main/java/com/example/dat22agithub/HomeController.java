5fd1f94a65f89b1c5f9cc294c4288e33e63ccd29
weix0015 <weix0015@stud.kea.dk>
2023-03-15 10:10:25 +0100

    2. commit from intelliJ

--- src/main/java/com/example/dat22agithub/HomeController.java
+++ src/main/java/com/example/dat22agithub/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("helloworld.com")
     public String helloWorld() {
         return "Hello World<br>你好地球<br>こんにちは世界";
     }
