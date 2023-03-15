d81957cfbbe7d0b473f688b9037e5677ed0cca46
weix0015 <weix0015@stud.kea.dk>
2023-03-15 09:00:29 +0100

    Initial commit weiyang

---
+++ src/main/java/com/example/dat22agithub/Dat22agithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.dat22agithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat22agithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dat22agithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dat22agithub/Dat22agithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dat22agithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat22agithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
